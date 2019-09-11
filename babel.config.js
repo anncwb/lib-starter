4802f244d34ac91c83f3d455ca8f4234097c293c
陈文彬 <anncwb@126.com>
2019-09-11 15:40:44 +0800

    perf: 去除无用的文件

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,9 @@
 var { clearConsole } = require('./config/index')
 
-var plugins = [
-  [
-    'component',
-    {
-      libraryName: 'element-ui',
-      styleLibraryName: 'theme-chalk'
-    }
-  ],
-  '@babel/plugin-syntax-dynamic-import'
-]
+var plugins = []
 clearConsole && plugins.push('transform-remove-console')
 module.exports = {
-  presets: [
-    '@vue/app',
-    [
-      '@vue/babel-preset-jsx',
-      {
-        injectH: false
-      }
-    ]
-  ],
+  presets: ['@babel/preset-env'],
 
   plugins: plugins
 }
